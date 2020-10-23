b46ed79b25e170108e243bcf6e1349e66287ae62
Thiago Aquino <thiagoaquiino@gmail.com>
2020-10-23 12:24:20 -0300

    add product service

--- facilit/src/main/java/com/facilit/controller/productController.java
+++ facilit/src/main/java/com/facilit/controller/productController.java
@@ -2,6 +2,7 @@ package com.facilit.controller;
 
 import com.facilit.model.Product;
 import com.facilit.repository.productRepository;
+import com.facilit.service.productService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,62 +12,38 @@ import javax.validation.Valid;
 public class productController {
 
     @Autowired
-    private productRepository pdr;
+    private productService ps;
 
     @RequestMapping(value = "/products", method = RequestMethod.POST)
     public @ResponseBody
-    Product productSave(@RequestBody Product product) throws Exception {
-        try {
-            pdr.save(product);
-            return product;
-        } catch (Exception e) {
-            throw new Exception("Check the product");
-        }
+    Product productSave(@RequestBody Product product) {
+        Product prod = ps.productSave(product);
+        return prod;
     }
 
     @RequestMapping(value = "/products", method = RequestMethod.GET)
     public Iterable<Product> productList() {
-        Iterable<Product> products = pdr.findAll();
+        Iterable<Product> products = ps.productList();
         return products;
     }
 
     @RequestMapping(value = "/products/{productCode}", method = RequestMethod.GET)
-    public Product productOnly(@PathVariable("productCode") long productCode) throws Exception {
-        try {
-            Product product = pdr.findByProductCode(productCode);
-            return product;
-        } catch (Exception e) {
-            throw new Exception("Check the product code");
-        }
+    public Product productOnly(@PathVariable("productCode") long productCode) {
+        Product product = ps.productOnly(productCode);
+        return product;
     }
 
     @RequestMapping(value = "/products/{productCode}", method = RequestMethod.DELETE)
     public Product productDelete(@PathVariable("productCode") long productCode) throws Exception {
-        try {
-            Product product = pdr.findByProductCode(productCode);
-            pdr.delete(product);
-            return product;
-        } catch (Exception e) {
-           throw new Exception("Check the product code");
-         }
+        Product product = ps.productDelete(productCode);
+        return product;
     }
+
 
     @RequestMapping(value = "/products/{productCode}", method = RequestMethod.PUT)
     public @ResponseBody
     Product productUpdate(@PathVariable("productCode") long productCode, @Valid @RequestBody Product product) throws Exception {
-        try {
-            Product prod = pdr.findByProductCode(productCode);
-            prod.setPriceProduct(product.getPriceProduct());
-            prod.setType(product.getType());
-            pdr.save(prod);
-            return prod;
-        } catch (Exception e) {
-            throw new Exception("Check the product code");
-        }
-    }
-
-    protected Product productOnlyType(String type) {
-        Product product = pdr.findByType(type);
-        return product;
+        Product prod = ps.productUpdate(productCode, product);
+        return prod;
     }
 }
--- facilit/src/main/java/com/facilit/service/productService.java
+++ facilit/src/main/java/com/facilit/service/productService.java
@@ -1,4 +1,53 @@
 package com.facilit.service;
 
+import com.facilit.model.Cart;
+import com.facilit.model.Item;
+import com.facilit.model.Product;
+import com.facilit.model.User;
+import com.facilit.repository.productRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class productService {
+
+    @Autowired
+    public productRepository pr;
+
+
+    public Product productOnly(long productCode) {
+        Product product = pr.findByProductCode(productCode);
+        return product;
+    }
+
+    public Iterable<Product> productList() {
+        Iterable<Product> products = pr.findAll();
+        return products;
+    }
+
+    public Product productSave(Product product) {
+        pr.save(product);
+        return product;
+    }
+
+    public Product productDelete(long productCode) {
+            Product product = pr.findByProductCode(productCode);
+            pr.delete(product);
+            return product;
+    }
+
+    public Product productUpdate(long productCode, Product product) {
+            Product prod = pr.findByProductCode(productCode);
+            prod.setPriceProduct(product.getPriceProduct());
+            prod.setType(product.getType());
+            pr.save(prod);
+            return prod;
+    }
+
+    public Product productOnlyType(String type) {
+        Product product = pr.findByType(type);
+        return product;
+    }
+
 }
